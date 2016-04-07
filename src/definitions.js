23d1f1366a0e8daee7619e15fd193fcbcdfa0763
ghoullier <gregory.houllier@gmail.com>
2016-04-07 11:54:15 +0200

    feat(definitions) Add service definitions

---
+++ src/definitions.js
@@ -0,0 +1,21 @@
+/**
+ * @desc Echo Service API
+ * @access public
+ */
+export const EchoPublisherDefinition = {
+  echo() { }
+}
+
+/**
+ * @desc Stack Service API
+ * @access public
+ */
+export const StackPublisherDefinition = {
+  getListeners() { },
+  list() { },
+  purge() { },
+  push() { },
+  remove() { },
+  setListeners() { },
+  update() { }
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
+import * as definitions from './definitions'
+
 export { AuthentFactory } from './handshake'
 export { API_URL, Client } from './client'
 export { SmartClient } from './smart-client'
 export { AbstractTokenPersistenceStrategy, LocalStorageTokenPersistenceStrategy } from './token-persistence'
+export { definitions }
