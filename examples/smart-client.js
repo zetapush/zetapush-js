a337238cac690d1168dc848a108a8cdbc77f5ea6
ghoullier <gregory.houllier@gmail.com>
2016-04-25 14:01:28 +0200

    feat(examples) Update client

--- examples/smart-client.js
+++ examples/smart-client.js
@@ -1,17 +1,15 @@
-{
-  const client = new ZetaPush.SmartClient({
-    businessId: '5mln3Zxw',
-    authenticationDeploymentId: 'VMuM'
-  })
+const client = new ZetaPush.SmartClient({
+  businessId: '5mln3Zxw',
+  authenticationDeploymentId: 'VMuM'
+})
 
-  client.addConnectionStatusListener({
-    onSuccessfulHandshake(authentication) {
-      console.debug('onSuccessfulHandshake', authentication)
-    },
-    onConnectionEstablished() {
-      console.debug('onConnectionEstablished')
-    }
-  })
+client.addConnectionStatusListener({
+  onSuccessfulHandshake(authentication) {
+    console.debug('onSuccessfulHandshake', authentication)
+  },
+  onConnectionEstablished() {
+    console.debug('onConnectionEstablished')
+  }
+})
 
-  client.connect()
-}
+client.connect()
