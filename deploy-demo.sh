f437bd43dcaee46bb13c0d565f9fd41472dbf236
ghoullier <gregory.houllier@gmail.com>
2016-05-09 09:32:20 +0200

    chore(build) Clean build scripts

--- deploy-demo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-npm run build
-
-scp -r ./dist/** root@51.255.201.174:/var/www/demo_zetapush/sdk-js-2.0.0-alpha.6/dist/
-scp -r ./docs/** root@51.255.201.174:/var/www/demo_zetapush/sdk-js-2.0.0-alpha.6/docs/
-scp -r ./examples/** root@51.255.201.174:/var/www/demo_zetapush/sdk-js-2.0.0-alpha.6/examples/
--- deploy-static.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-npm run build
-scp ./dist/zetapush.js ubuntu@54.171.164.234:/var/www/static.zpush.io/js/2.0.0-alpha.6/
