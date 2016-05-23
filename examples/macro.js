8cc5f40dd2199fc0ee6cda69cae99a151fd04978
ghoullier <gregory.houllier@gmail.com>
2016-05-23 16:16:55 +0200

    feat(connection) Auto renew subscription on reconnection

--- examples/macro.js
+++ examples/macro.js
@@ -19,14 +19,28 @@ const { publisher } = client.createServicePublisherSubscriber({
 client.addConnectionStatusListener({
   onConnectionEstablished() {
     console.debug('onConnectionEstablished')
+  },
+  onConnectionClosed() {
+    console.debug('onConnectionClosed')
+  }
+})
 
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('.js-SayHello').addEventListener('click', () => {
+    console.log('.js-SayHello', 'click')
     publisher.call({
       name: 'hello',
       parameters: {
         value: 'World'
       }
     })
-  }
+  })
+  document.querySelector('.js-Connect').addEventListener('click', () => {
+    console.log('.js-Connect', 'click')
+    client.connect()
+  })
+  document.querySelector('.js-Disconnect').addEventListener('click', () => {
+    console.log('.js-Disconnect', 'click')
+    client.disconnect()
+  })
 })
-
-client.connect()
