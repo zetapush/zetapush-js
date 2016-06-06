d764555e9b4eed1cbe98b757a1d5aadd045628ba
Grégory Houllier <gregory.houllier@gmail.com>
2016-06-06 11:04:11 +0200

    feat(examples) Update service definition

--- examples/app/todo.js
+++ examples/app/todo.js
@@ -23,59 +23,57 @@
     fragment.appendChild(form)
     return wrapper ? dom('li', { 'class': completed ? 'completed' : '', 'data-guid': guid }, fragment) : fragment
   }
-  // Declare a service listner mapping stack methods
-  const stackServiceListener = {
-    // Triggered when api return list of stack elements
-    list({ channel, data }) {
-      const { result: { content } } = data
-      const fragment = document.createDocumentFragment()
-      content.map((item) => {
-        fragment.appendChild(getTodoItemDom(item))
-      })
-      document.querySelector('.todo-list').appendChild(fragment)
-    },
-    // Triggered when api has purged
-    purge({ channel, data })  {
-      const list = document.querySelector('.todo-list')
-      while (list.firstChild) {
-        list.removeChild(list.firstChild)
-      }
-    },
-    // Triggered when a new item is pushed
-    push({ channel, data })  {
-      const list = document.querySelector('.todo-list')
-      const todo = getTodoItemDom(data)
-      list.insertBefore(todo, list.firstChild)
-      document.querySelector('.new-todo').value = ''
-    },
-    // Triggered when an item is removed
-    remove({ channel, data }) {
-      const { guids = [] } = data
-      guids.forEach((guid) => {
-        const li = document.querySelector(`input[data-guid="${guid}"]`).parentNode.parentNode
-        li.parentNode.removeChild(li)
-      })
-    },
-    // Triggered when an item is updated
-    update({ channel, data }) {
-      const { guid } = data
-      const li = document.querySelector(`li[data-guid="${guid}"]`)
-      li.className = data.data.completed ? 'completed' : ''
-      while (li.firstChild) {
-        li.removeChild(li.firstChild)
-      }
-      const todo = getTodoItemDom(data, false)
-      li.appendChild(todo)
-      const items = Array.from(document.querySelectorAll('.todo-list li'))
-      items.forEach((item) => {
-        item.classList.remove('editing')
-      })
-    }
-  }
   // Create a service publish to interact with remote API
   const { publisher } = client.createServicePublisherSubscriber({
     definition: ZetaPush.definitions.StackPublisherDefinition,
-    listener: stackServiceListener
+    listener: {
+      // Triggered when api return list of stack elements
+      list({ channel, data }) {
+        const { result: { content } } = data
+        const fragment = document.createDocumentFragment()
+        content.map((item) => {
+          fragment.appendChild(getTodoItemDom(item))
+        })
+        document.querySelector('.todo-list').appendChild(fragment)
+      },
+      // Triggered when api has purged
+      purge({ channel, data })  {
+        const list = document.querySelector('.todo-list')
+        while (list.firstChild) {
+          list.removeChild(list.firstChild)
+        }
+      },
+      // Triggered when a new item is pushed
+      push({ channel, data })  {
+        const list = document.querySelector('.todo-list')
+        const todo = getTodoItemDom(data)
+        list.insertBefore(todo, list.firstChild)
+        document.querySelector('.new-todo').value = ''
+      },
+      // Triggered when an item is removed
+      remove({ channel, data }) {
+        const { guids = [] } = data
+        guids.forEach((guid) => {
+          const li = document.querySelector(`input[data-guid="${guid}"]`).parentNode.parentNode
+          li.parentNode.removeChild(li)
+        })
+      },
+      // Triggered when an item is updated
+      update({ channel, data }) {
+        const { guid } = data
+        const li = document.querySelector(`li[data-guid="${guid}"]`)
+        li.className = data.data.completed ? 'completed' : ''
+        while (li.firstChild) {
+          li.removeChild(li.firstChild)
+        }
+        const todo = getTodoItemDom(data, false)
+        li.appendChild(todo)
+        const items = Array.from(document.querySelectorAll('.todo-list li'))
+        items.forEach((item) => {
+          item.classList.remove('editing')
+        })
+      }
+    }
   })
   // Add listener to life cycle connection events
   client.onConnectionEstablished(() => {
