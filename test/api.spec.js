a8bba8d5e04439a49fd0cd34edb10306a7dda44e
Grégory Houllier <gregory.houllier@gmail.com>
2016-08-18 17:20:08 +0200

    feat(test) Add test case for services namespace

--- test/api.spec.js
+++ test/api.spec.js
@@ -2,10 +2,13 @@ describe('Public API', function () {
   it('Should define a ZetaPush global object', function () {
     expect(typeof ZetaPush).toBe('object')
   })
-  it('Should define a ZetaPush.Client global object', function () {
+  it('Should define a ZetaPush.Client object', function () {
     expect(typeof ZetaPush.Client).toBe('function')
   })
-  it('Should define a ZetaPush.WeakClient global object', function () {
+  it('Should define a ZetaPush.WeakClient object', function () {
     expect(typeof ZetaPush.WeakClient).toBe('function')
+  })
+  it('Should define a ZetaPush.services object', function () {
+    expect(typeof ZetaPush.services).toBe('object')
   })
 })
