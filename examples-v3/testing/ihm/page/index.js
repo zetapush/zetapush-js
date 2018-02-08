4389c82c1d42d7a7594436f976cdff4606c1f939
damienld22 <damien-le-dantec@hotmail.fr>
2018-02-08 10:27:52 +0100

    feat(launch all tests from IHM)

--- examples-v3/testing/ihm/page/index.js
+++ examples-v3/testing/ihm/page/index.js
@@ -1,12 +1,21 @@
 // TESTS
 function startAllTests() {
     const req = new XMLHttpRequest();
+
+    req.onreadystatechange = function() {
+        if (req.readyState === 4) {
+            console.log('RESPONSE', req.response)
+        }
+    }
+
     req.open('GET', 'http://localhost:8080/tests/startAll', false)
     req.send(null)
 
     if (req.status === 200) {
-        console.log('Start all tests OK')
+        console.log('Start all tests')
     } else {
         console.log('Error to start all tests')
     }
+
+    
 }
--- examples-v3/testing/ihm/server/api/controllers/controlTestsController.js
+++ examples-v3/testing/ihm/server/api/controllers/controlTestsController.js
@@ -1,6 +1,28 @@
 'use strict';
 
+const { spawn } = require('child_process');
+
+
 exports.startAllTests = function(req, res) {
     console.log('Start all tests...')
-    res.send('Start all tests')
+    const result = []
+
+    const startAllTests = spawn('../../../node_modules/.bin/mocha', ['../../mocha/', '--exit']);
+
+    //child.stdout.setEncoding('utf8');
+    startAllTests.stdout.on('data', (chunk) => {
+        result.push(chunk.toString('utf-8'))
+    });
+
+
+    startAllTests.on('close', (code) => {
+        console.log(`startAllTests process exited with code : ${code}`)
+        if (result.length > 0) {
+            console.log('Result of startAllTests sent to client')
+            res.end(JSON.stringify(result))
+        } else {
+            console.warn('No result for startAllTests process')
+            res.end('No result for startAllTests process')
+        }
+    })
 }
